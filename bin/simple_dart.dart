a5ac44cf26a202d24c9c888b7b43072409ac5254
morikann <mrknt0088@gmail.com>
2024-02-29 16:06:18 +0900

    init

---
+++ bin/simple_dart.dart
@@ -0,0 +1,5 @@
+import 'package:simple_dart/simple_dart.dart' as simple_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${simple_dart.calculate()}!');
+}
---
+++ test/simple_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:simple_dart/simple_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
